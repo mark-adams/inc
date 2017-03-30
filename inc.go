8630c8c4fc51d1c901cfa808c135a90c870c79e7
Mark Adams <mark@markadams.me>
2017-03-30 15:16:31 -0500

    Switched to using chi for the mux

--- inc.go
+++ inc.go
@@ -11,12 +11,13 @@ import (
 	statsd "gopkg.in/alexcesaro/statsd.v2"
 
 	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"github.com/mark-adams/inc/backends"
+	"github.com/pressly/chi"
 )
 
-var app *mux.Router
+var app *chi.Mux
+var metrics MetricCollector
 
 func getRandomID() (string, error) {
 	newID := make([]byte, 16)
@@ -28,102 +29,91 @@ func getRandomID() (string, error) {
 	return hex.EncodeToString(newID), nil
 }
 
-func init() {
-	app = mux.NewRouter()
+// NewToken creates a new counting token
+func NewToken(w http.ResponseWriter, r *http.Request) {
+	metrics.Increment("inc.api.create_token")
 
-	metrics, err := statsd.New(statsd.Address(os.Getenv("STATSD_HOST")))
+	id, err := getRandomID()
+	if err != nil {
+		log.Printf("Error: %s", err)
+		http.Error(w, "😞 Something bad happened... try again?", http.StatusInternalServerError)
+		return
+	}
+	db, err := backends.GetBackend()
+	if err != nil {
+		log.Printf("Database error: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	err = db.CreateToken(id)
+	if err != nil {
+		log.Printf("Insert error: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, id)
+}
+
+// IncrementToken increments an existing token
+func IncrementToken(w http.ResponseWriter, r *http.Request) {
+	metrics.Increment("inc.api.increment_token")
+	db, err := backends.GetBackend()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	count, err := db.IncrementAndGetToken(chi.URLParam(r, "token"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%d", count)
+}
+
+// IncrementTokenNamespace increments a specific namespace inside a specific token's context
+func IncrementTokenNamespace(w http.ResponseWriter, r *http.Request) {
+	metrics.Increment("inc.api.increment_namespace_token")
+	db, err := backends.GetBackend()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	count, err := db.IncrementAndGetNamespacedToken(chi.URLParam(r, "token"), chi.URLParam(r, "namespae"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%d", count)
+}
+
+func init() {
+	var err error
+
+	app = chi.NewRouter()
+	metrics = &NullMetricsCollector{}
+	metrics, err = statsd.New(statsd.Address(os.Getenv("STATSD_HOST")))
+
 	if err != nil && os.Getenv("STATSD_HOST") != "" {
 		log.Printf("error initializing metrics: %s", err)
 	}
 
-	app.Path("/_healthcheck").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
+	app.Get("/_healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "OK")
 	})
 
-	app.Path("/new").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-
-		metrics.Increment("inc.api.create_token")
-
-		id, err := getRandomID()
-		if err != nil {
-			log.Printf("Error: %s", err)
-			http.Error(w, "😞 Something bad happened... try again?", http.StatusInternalServerError)
-			return
-		}
-		db, err := backends.GetBackend()
-		if err != nil {
-			log.Printf("Database error: %s", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer db.Close()
-
-		err = db.CreateToken(id)
-		if err != nil {
-			log.Printf("Insert error: %s", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(id))
-	})
-
-	app.Path("/{token:[a-zA-Z0-9]{32}}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "PUT" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-
-		metrics.Increment("inc.api.increment_token")
-		params := mux.Vars(r)
-		db, err := backends.GetBackend()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer db.Close()
-
-		count, err := db.IncrementAndGetToken(params["token"])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, "%d", count)
-	})
-
-	app.Path("/{token:[a-zA-Z0-9]{32}}/{namespace:.+}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		if r.Method != "PUT" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-
-		metrics.Increment("inc.api.increment_namespace_token")
-		params := mux.Vars(r)
-		db, err := backends.GetBackend()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer db.Close()
-
-		count, err := db.IncrementAndGetNamespacedToken(params["token"], params["namespace"])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, "%d", count)
-
+	app.Post("/new", NewToken)
+	app.Route("/:token", func(r chi.Router) {
+		r.Put("/", IncrementToken)
+		r.Put("/:namespace", IncrementTokenNamespace)
 	})
 }
 
---
+++ utils.go
@@ -0,0 +1,14 @@
+package main
+
+// MetricCollector reports application metrics
+type MetricCollector interface {
+	Increment(bucket string)
+}
+
+// NullMetricsCollector is a stub metric reporter that doesn't do anything with the metrics
+type NullMetricsCollector struct{}
+
+// Increment increases a metric
+func (n *NullMetricsCollector) Increment(bucket string) {
+	// Don't do anything
+}
